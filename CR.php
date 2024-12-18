37c76985e0bf24994a3c6fc2281176b20bfda26e
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2024-12-18 09:55:56 +0100

    Read
    -read function
    -home test prototype

--- CR.php
+++ CR.php
@@ -54,4 +54,42 @@ class CR {
                 exit(); // Zorg ervoor dat het script stopt na de header redirect
         }
     }
+    public function read(){
+        try{
+            $connectie = new \mysqli($this->serverConnectieData[0], 
+            $this->serverConnectieData[1],
+            $this->serverConnectieData[2],
+            $this->serverConnectieData[3]);
+            if ($connectie->error)
+            {
+                throw new \Exception($connectie->connect_error);
+            }
+            $query = "SELECT * FROM gebruikers";
+            //Bereid de SQL-query voor en bind de parameters.
+            $statement = $connectie->prepare($query);
+            // Voer de query uit en controleer op fouten
+            if (!$statement->execute())
+            {
+                throw new \Exception($connectie->error);
+            }
+        }
+        catch(\Exception $e)
+        {
+            //Als er fouten zijn, komt de volgende melding: 
+            echo "<div class='alert alert-warning'><h4>Oops: Is het iets met de Server!</h4></div>"; //. $e->getMessage();
+        }
+        finally
+        {
+                // Sluit de statement en de connectie
+                if($statement){
+                    $statement->close();
+                } 
+                if($connectie){
+                    $connectie->close();
+                }
+                // Redirect de gebruiker naar de login pagina
+                // header("location: login.php?register=true");
+                exit(); // Zorg ervoor dat het script stopt na de header redirect
+        }
+    }
 }
