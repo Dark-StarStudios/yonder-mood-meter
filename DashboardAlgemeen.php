c53a9d69a28c03ba6539b05e39b06c50752297c5
DeusOdio <ivothoen@outlook.com>
2025-01-09 11:52:55 +0100

    Dashboard admin
    begin aan Dashboard admin

---
+++ DashboardAlgemeen.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dashboard algemeen</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/DashboardAlgemeen.css">
+</head>
+</head>
+<body>
+<img src="../img/logo-yonder.svg" alt="Logo Yonder" class="logo">
+    <H1>dashboard</H1>
+     <div class="container-fluid d-flex flex-column justify-content-between" style="height: 71vh;">
+        <div class="row">
+            <div class="col-8" ></div>
+            <div class="col-4 bg-paars" ></div>
+        </div> 
+        <!-- 
+        algemeen (paars)
+        gebruikers 
+        admins
+        -->
+
+        <div class="row">
+            <div class="col-4 bg-rood" ></div>
+            <div class="col-8" ></div>
+            <div class="col-4" ></div>
+            <div class="col-8 bg-zwart" ></div>
+        </div> 
+     </div>
+    <!-- div balk 2 paars -->
+     <!-- div balk 3  rood -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
