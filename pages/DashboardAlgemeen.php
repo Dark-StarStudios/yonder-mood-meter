6034ca7f3e506cee59f97b2d7ad4b2bcc1730b8a
DeusOdio <ivothoen@outlook.com>
2025-01-09 12:03:19 +0100

    Dashboard update

--- pages/DashboardAlgemeen.php
+++ pages/DashboardAlgemeen.php
@@ -10,21 +10,17 @@
 </head>
 <body>
 <img src="../img/logo-yonder.svg" alt="Logo Yonder" class="logo">
-    <H1>dashboard</H1>
+    <H1 class="dashboardH1">dashboard</H1>
      <div class="container-fluid d-flex flex-column justify-content-between" style="height: 71vh;">
         <div class="row">
             <div class="col-8" ></div>
             <div class="col-4 bg-paars" ></div>
         </div> 
-        <!-- 
-        algemeen (paars)
-        gebruikers 
-        admins
-        -->
+
         <h1>algemeen</h1>
         <h1>gebruikers</h1>
         <h1>admin</h1>
-        
+
         <div class="row">
             <div class="col-4 bg-rood" ></div>
             <div class="col-8" ></div>
@@ -32,8 +28,6 @@
             <div class="col-8 bg-zwart" ></div>
         </div> 
      </div>
-    <!-- div balk 2 paars -->
-     <!-- div balk 3  rood -->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
