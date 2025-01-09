a3bf8fdea45438eb9e0134f583fe483bab75ea2e
DeusOdio <ivothoen@outlook.com>
2025-01-09 11:58:30 +0100

    Dashboard update

--- pages/DashboardAlgemeen.php
+++ pages/DashboardAlgemeen.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>dashboard algemeen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/DashboardAlgemeen.css">
+    <link rel="stylesheet" href="../css/dashboardAlgemeen.css">
 </head>
 </head>
 <body>
@@ -21,7 +21,10 @@
         gebruikers 
         admins
         -->
-
+        <h1>algemeen</h1>
+        <h1>gebruikers</h1>
+        <h1>admin</h1>
+        
         <div class="row">
             <div class="col-4 bg-rood" ></div>
             <div class="col-8" ></div>
